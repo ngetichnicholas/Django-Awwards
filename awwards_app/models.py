f40af0e18d912ae980624a744ad38b20aee230c9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 13:48:17 +0300

    Add project class model

--- awwards_app/models.py
+++ awwards_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Project(models.Model):
+  title = models.CharField(max_length=144)
+  description = models.TextField()
+  published_at = models.DateTimeField(auto_now_add=True)
+  project_image = CloudinaryField('image')
+  repo_link = models.CharField(max_length=144)
+  live_link = models.CharField(max_length=144)
