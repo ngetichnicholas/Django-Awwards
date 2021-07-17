803b0a2930c2c3237b30111b50ab7f49b1977d23
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 11:28:48 +0300

    Add app urls

---
+++ awwards_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.urls import re_path,path
+from django.contrib.auth import views as auth_views
+from . import views as app_views
+
+urlpatterns = [
+  path('index/',app_views.index,name='home'),
+  path('accounts/register/',app_views.register,name='register'),
+  path('',auth_views.LoginView.as_view(template_name = 'registration/login.html'),name='login'),
+  path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html'),name='logout'),
+  path('accounts/profile/',app_views.profile,name='profile'),
+]
