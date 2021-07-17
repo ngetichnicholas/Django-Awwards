95d4e53891b8bff9319ddfd19d238e0f08f8f1dd
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 14:00:35 +0300

    Add profile class model

--- awwards_app/models.py
+++ awwards_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import datetime as dt
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+
 
 # Create your models here.
 class Project(models.Model):
@@ -10,3 +13,11 @@ class Project(models.Model):
   project_image = CloudinaryField('image')
   repo_link = models.CharField(max_length=144)
   live_link = models.CharField(max_length=144)
+
+class Profile(models.Model):
+  profile_picture = CloudinaryField('image')
+  bio = models.TextField()
+  user = models.OneToOneField(User,on_delete = models.CASCADE)
+  email = models.EmailField()
+  phone = models.CharField(max_length=10)
+  address = models.CharField(max_length=30)
