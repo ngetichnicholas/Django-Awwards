af8830834c44b788e6f829a877cd702e5f2a9775
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 11:35:46 +0300

    Add index view

--- awwards_app/views.py
+++ awwards_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+@login_required
+def index(request):
+  all_users = User.objects.all()
+  
+  return render (request,'index.html',{"all_users":all_users})
