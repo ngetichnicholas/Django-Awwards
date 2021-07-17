2bf08fe358dc9b2065d53a41449e698feefd11f3
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 11:36:57 +0300

    Add register view

--- awwards_app/views.py
+++ awwards_app/views.py
@@ -10,3 +10,17 @@ def index(request):
   all_users = User.objects.all()
   
   return render (request,'index.html',{"all_users":all_users})
+
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('login')
+  else:
+    form = Registration()
+  return render(request,'registration/registration_form.html',{"form":form})
