af0cfdb1244c250111c70e57cd025400f86d04b4
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-17 16:42:56 +0300

    Update rate class

--- awwards_app/models.py
+++ awwards_app/models.py
@@ -9,10 +9,10 @@ from django.db.models.deletion import CASCADE
 class Project(models.Model):
   title = models.CharField(max_length=144)
   description = models.TextField()
-  published_at = models.DateTimeField(auto_now_add=True)
+  posted_on = models.DateTimeField(auto_now_add=True)
   project_image = CloudinaryField('image')
-  repo_link = models.CharField(max_length=144)
-  live_link = models.CharField(max_length=144)
+  repo_link = models.URLField(max_length=300)
+  live_link = models.URLField(max_length=300)
   user = models.ForeignKey(User,on_delete = models.CASCADE)
 
 
@@ -28,4 +28,6 @@ class Rate(models.Model):
   content_wise = models.IntegerField(blank=True,default=0)
   usability_wise = models.IntegerField(blank=True,default=0)
   design_wise = models.IntegerField(blank=True,default=0)
+  average_rate = models.FloatField(default=0.0,blank=True)
   project = models.ForeignKey(Project,on_delete=CASCADE)
+  user = models.ForeignKey(User,on_delete=CASCADE)
