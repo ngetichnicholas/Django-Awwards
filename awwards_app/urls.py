9781f54d017fa60240e413b5516e33d90f19df18
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-19 23:39:13 +0300

    Update authentication required views

--- awwards_app/urls.py
+++ awwards_app/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 from . import views as app_views
 
 urlpatterns = [
-  path('index/',app_views.index,name='home'),
+  path('',app_views.index,name='home'),
   path('accounts/register/',app_views.register,name='register'),
-  path('',auth_views.LoginView.as_view(template_name = 'registration/login.html'),name='login'),
+  path('login/',auth_views.LoginView.as_view(template_name = 'registration/login.html'),name='login'),
   path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html'),name='logout'),
   path('accounts/profile/',app_views.profile,name='profile'),
   path('update/',app_views.update_profile,name='update_profile'),
