c787bfa18749f46f2e2ae2fcbc7d101940d97526
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-20 12:51:47 +0300

    Add serializer

---
+++ awwards_app/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Profile, Project
+from django.contrib.auth.models import User
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'user', 'phone','address']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'posted_on','project_image','repo_link','live_link','user' 'technologies_used']
+
