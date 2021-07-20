9572dedfeb7cec00d216c5cc2971223796a1fd63
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-20 12:05:44 +0300

    Add rating tests

--- awwards_app/tests.py
+++ awwards_app/tests.py
@@ -4,6 +4,24 @@ from .models import Rate,Profile,Project
 from django.contrib.auth.models import User
 
 
+class RateTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='NgetichNIck')
+        self.project = Project.objects.create(id=1, title='Moringa Project', description='Moringa Project description',posted_on='2021,6,19',project_image='https://cloudinary url', repo_link='http://github.com',live_link='http://heroku.com',user=self.user,technologies_used='Python')
+        self.rating = Rate.objects.create(id=1, design_wise=5, usability_wise=8, content_wise=6, user=self.user, project=self.project)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rate))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rate.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_ratings(self, id):
+        self.rating.save()
+        rating = Rate.get_ratings(post_id=id)
+        self.assertTrue(len(rating) == 1)
 
 class TestProfile(TestCase):
     def setUp(self):
