ac89ccab4e2a92954cfeef475c194f272186973e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-20 11:51:12 +0300

    Add profile test

--- awwards_app/tests.py
+++ awwards_app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+from .models import Rate,Profile,Project
+from django.contrib.auth.models import User
+
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='NgetichNick', password='Moringa')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
